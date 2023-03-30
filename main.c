9ed814f310c3c78dae21456c2797c5ff2332a960
IsmaelMiguez <i.miguezv@udc.es>
2023-03-30 16:46:54 +0200

    Ejercicio1_V1

--- main.c
+++ main.c
@@ -27,23 +27,61 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    int i, n, count=0;
+    MPI_Init(&argc, &argv);//inicializamos que los procesos se comuniquen
+
+    int n, size,rank, count = 0;
     char *cadena;
     char L;
 
-    n = atoi(argv[1]);
-    L = *argv[2];
+    MPI_Comm_size(MPI_COMM_WORLD, &size);// numero de procesos
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);//devuelve el rango de cada proceso
 
     cadena = (char *) malloc(n*sizeof(char));
-    inicializaCadena(cadena, n);
 
-    for(i=0; i<n; i++){
+    if(rank==0){
+        n = atoi(argv[1]);//tamaño de la cadena
+        L = *argv[2];//Letra a contar
+
+        for(int j = 1 ; j< size; j++){//bucle para envio de argumentos
+
+            MPI_Send(&n,1,MPI_INT, j, MPI_ANY_TAG, MPI_COMM_WORLD );//enviamos el tamaño de la cadena
+            MPI_Send(&L,1, MPI_CHAR, j, MPI_ANY_TAG, MPI_COMM_WORLD );//enviamos el char
+        }
+    }
+    else{
+        MPI_Recv(&n,1,MPI_INT, 0,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);//cada proceso recibe n y L
+        MPI_Recv(&L,1,MPI_CHAR, 0,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    }
+
+    inicializaCadena(cadena, n);  //inicializamos la cadena
+
+    //operacion de conteo
+    for(int i=rank; i<n; i+size){
         if(cadena[i] == L){
             count++;
         }
     }
 
-    printf("El numero de apariciones de la letra %c es %d\n", L, count);
+    if(rank==0){
+        int aux;
+        for(int j = 1 ; j< size; j++){//recepcion del contador
+
+            MPI_Recv(&aux,1,MPI_INT, j,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);//recibimos de cada particion
+        }
+        //suma
+
+        count = count + aux;
+        printf("El numero de apariciones de la letra %c es %d\n", L, count);
+
+    }
+    else{
+        MPI_Send(&count,1,MPI_INT, 0,MPI_ANY_TAG, MPI_COMM_WORLD );//cada proceso (menos el 0) envia su contador
+
+    }
+
+
+
+    MPI_Finalize();	// se cierra la tarea dividida
     free(cadena);
     exit(0);
 }
