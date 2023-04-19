e96f59f45eb4c58ced00268de3cf9e7caef8e31d
i.miguezv <i.miguezv@udc.es>
2023-04-19 19:17:42 +0200

    Practica1_vFinal

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <mpi.h>
 
-
+//InicializaciÃ³n de la cadena a tratar en el problema.
 void inicializaCadena(char *cadena, int n){
     int i;
     for(i=0; i<n/2; i++){
@@ -20,30 +20,75 @@ void inicializaCadena(char *cadena, int n){
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+    //ComprobaciÃ³n de que se pasen los parametros suficientes para la ejecuciÃ³n del programa
     if(argc != 3){
-        printf("Numero incorrecto de parametros\nLa sintaxis debe ser: program n L\n  program es el nombre del ejecutable\n  n es el tamaño de la cadena a generar\n  L es la letra de la que se quiere contar apariciones (A, C, G o T)\n");
+        printf("Numero incorrecto de parametros\nLa sintaxis debe ser: program n L\n  program es el nombre del ejecutable\n  n es el tamaÃ±o de la cadena a generar\n  L es la letra de la que se quiere contar apariciones (A, C, G o T)\n");
         exit(1);
     }
 
-    int i, n, count=0;
+    //DeclaraciÃ³n de variables
+    int n, size,rank, count = 0;
     char *cadena;
     char L;
 
-    n = atoi(argv[1]);
-    L = *argv[2];
+    //InicializaciÃ³n del entorno MPI
+    MPI_Init(&argc, &argv);//inicializamos que los procesos se comuniquen
+    MPI_Comm_size(MPI_COMM_WORLD, &size);// numero de procesos
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);//devuelve el rango de cada proceso
 
+    //P0 es el que envÃ­a la informaciÃ³n a los otros procesos. Inicializavos las variables en proceso 0
+    if(rank==0){
+        n = atoi(argv[1]);//tamaÃ±o de la cadena
+        L = *argv[2];//Letra a contar
+
+        //Bucle para realizar el send y el recieved de las variables n y L
+        for(int j = 1 ; j<size; j++){//bucle para envio de argumentos
+            MPI_Send(&n,1,MPI_INT, j, 0, MPI_COMM_WORLD );//enviamos el tamaÃ±o de la cadena
+            MPI_Send(&L,1, MPI_CHAR, j, 0, MPI_COMM_WORLD );//enviamos el char
+
+        }
+    }
+        //recepciÃ³n de los argumentos n y L
+    else{
+        MPI_Recv(&n,1,MPI_INT, 0,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        MPI_Recv(&L,1,MPI_CHAR, 0,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+    }
+
+    // se realiza la inicializacion de la cadena
     cadena = (char *) malloc(n*sizeof(char));
-    inicializaCadena(cadena, n);
+    inicializaCadena(cadena, n);  //inicializamos la cadena
 
-    for(i=0; i<n; i++){
+    //operacion de conteo
+    for(int i=rank; i<n; i= i +size){
         if(cadena[i] == L){
             count++;
         }
     }
 
-    printf("El numero de apariciones de la letra %c es %d\n", L, count);
+    //bucle para el send y el recieved del contador de cada proceso. Se envÃ­a la info desde el resto de procesos a P0
+    if(rank==0){
+        int aux;
+        for(int j = 1 ; j<size; j++){
+            //recepcion del contador
+            MPI_Recv(&aux,1,MPI_INT, j,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            //Contador acumulado
+            count = count + aux;
+        }
+
+        //Print del resultado por P0
+        printf("El numero de apariciones de la letra %c es %d\n", L, count);
+
+    }
+    else{
+        //EnvÃ­o de contador, cada proceso (menos el 0) envia su contador
+        MPI_Send(&count,1,MPI_INT, 0,0, MPI_COMM_WORLD );
+    }
+
+    MPI_Finalize();	// se cierra la tarea dividida
     free(cadena);
     exit(0);
 }
+
+
